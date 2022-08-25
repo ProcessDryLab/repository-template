3028c1f927398bccd69e4f97fa2780cbb6e486c4
Andrea Burattin <andrea.burattin@gmail.com>
2022-08-25 15:15:47 +0200

    Added error controller and max file size upload

---
+++ src/main/java/pdl/repository/template/controllers/MyErrorController.java
@@ -0,0 +1,16 @@
+package pdl.repository.template.controllers;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class MyErrorController implements ErrorController {
+
+	@RequestMapping("/error")
+	public String handleError() {
+		return "";
+	}
+}
