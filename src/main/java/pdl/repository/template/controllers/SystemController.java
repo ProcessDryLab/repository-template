f4c825a70fbaee3472c6ad4b9e3bbc2d0bdd83d0
Andrea Burattin <andrea.burattin@gmail.com>
2022-08-26 13:03:57 +0200

    Added web.config and endpoint with version

--- src/main/java/pdl/repository/template/controllers/SystemController.java
+++ src/main/java/pdl/repository/template/controllers/SystemController.java
@@ -1,5 +1,7 @@
 package pdl.repository.template.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class SystemController {
 
+	@Autowired
+	BuildProperties buildProperties;
+
 	@GetMapping("/ping")
 	public @ResponseBody String ping() {
 		return "pong";
+	}
+
+	@GetMapping("/version")
+	public @ResponseBody String version() {
+		return buildProperties.getTime().toString();
 	}
 }
