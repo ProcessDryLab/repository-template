83a196f164a219941ebccd61b28c8d8f5daf44f7
Andrea Burattin <andrea.burattin@gmail.com>
2022-08-27 13:45:15 +0200

    Added bean to configure max upload file size

--- src/main/java/pdl/repository/template/Application.java
+++ src/main/java/pdl/repository/template/Application.java
@@ -1,11 +1,25 @@
 package pdl.repository.template;
 
+import javax.servlet.MultipartConfigElement;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.unit.DataSize;
 
 @SpringBootApplication
 public class Application {
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		factory.setMaxFileSize(DataSize.ofBytes(100000000L));
+		factory.setMaxRequestSize(DataSize.ofBytes(100000000L));
+		return factory.createMultipartConfig();
 	}
 }
